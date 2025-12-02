80707254508b3139b8ff8e9f8e08d911294eea8c
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-12-02 14:48:07 -0500

    Implemented features relating to private buckets and serving files through a CDN.

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -45,14 +45,81 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const videoFullName = `${cfg.assetsRoot}/${videoFileName}.${mime.extension(videoFileType)}`;
   Bun.write(videoFullName, await videoFile.arrayBuffer());
 
-  const s3File = cfg.s3Client.file(
-    `${videoFileName}.${mime.extension(videoFileType)}`,
-  );
-  s3File.write(Bun.file(videoFullName), { type: videoFileType });
+  const ratio = await getVideoAspectRatio(videoFullName);
+  const processed = await processVideoForFastStart(videoFullName);
+
+  const s3Path = `videos/${ratio}/${videoFileName}.${mime.extension(videoFileType)}`;
+  const s3File = cfg.s3Client.file(s3Path);
+  s3File.write(Bun.file(processed), { type: videoFileType });
 
   Bun.file(videoFullName).delete();
-  const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${videoFileName}.${mime.extension(videoFileType)}`;
-  video.videoURL = videoURL;
+  Bun.file(processed).delete();
+
+  video.videoURL = `${cfg.s3CfDistribution}/${s3Path}`;
   updateVideo(cfg.db, video);
   return respondWithJSON(200, video);
+}
+
+async function getVideoAspectRatio(filePath: string) {
+  const proc = Bun.spawn(
+    [
+      "ffprobe",
+      "-v",
+      "error",
+      "-select_streams",
+      "v:0",
+      "-show_entries",
+      "stream=width,height",
+      "-of",
+      "json",
+      filePath,
+    ],
+    {
+      stdout: "pipe",
+      stderr: "pipe",
+    },
+  );
+  await proc.exited;
+
+  let output, err;
+  const exitCode = proc.exitCode;
+  if (exitCode !== 0) {
+    err = await new Response(proc.stderr).json();
+    throw new Error(`Server Error: Processing Aspect Ratio ${err}`);
+  }
+  output = await new Response(proc.stdout).json();
+
+  const videoHeight = output.streams[0].height,
+    videoWidth = output.streams[0].width;
+  const ratio = videoWidth / videoHeight;
+
+  if (ratio > 1.7 && ratio < 1.8) {
+    return "landscape";
+  } else if (ratio > 0.5 && ratio < 0.6) {
+    return "portrait";
+  } else {
+    return "other";
+  }
+}
+
+async function processVideoForFastStart(inputFilePath: string) {
+  const outputFilePath = `${inputFilePath}.processed`;
+
+  const proc = Bun.spawn([
+    "ffmpeg",
+    "-i",
+    inputFilePath,
+    "-movflags",
+    "faststart",
+    "-map_metadata",
+    "0",
+    "-codec",
+    "copy",
+    "-f",
+    "mp4",
+    outputFilePath,
+  ]);
+  await proc.exited;
+
+  return outputFilePath;
 }
